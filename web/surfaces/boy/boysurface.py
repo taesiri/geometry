09270bafe3f657226b683e238d7fd0c98a5d71e6
Mohammadreza <live@taesiri.com>
2013-04-30 21:28:35 +0430

    Boy Surface

---
+++ web/surfaces/boy/boysurface.py
@@ -0,0 +1,5 @@
+u, v = var('u,v')
+fx = 2/3* (cos(u)* cos(2*v) + sqrt(2)* sin(u)* cos(v))* cos(u) / (sqrt(2) - sin(2*u)* sin(3*v))
+fy = 2/3* (cos(u)* sin(2*v) - sqrt(2)* sin(u)* sin(v))* cos(u) / (sqrt(2) - sin(2*u)* sin(3*v))
+fz = sqrt(2)* cos(u)* cos(u) / (sqrt(2) - sin(2*u)* sin(3*v))
+parametric_plot3d([fx, fy, fz], (u, -2*pi, 2*pi), (v, 0, pi), plot_points = [90,90], frame=False, color="orange") # long time -- about 30 seconds
